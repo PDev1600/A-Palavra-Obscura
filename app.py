eec4ef1923750f3e61570b01688b8b564285aefd
PDev1600 <fgondarmoreira@gmail.com>
2025-08-16 21:11:46 +0100

    Atualizei app.py para Render

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def guess():
     return redirect(url_for("game"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
